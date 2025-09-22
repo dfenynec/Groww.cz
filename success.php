c3baccb6f10b5f56e178b7ecc1d6f8d6b8cd2216
dfenynec <160599407+dfenynec@users.noreply.github.com>
2025-09-22 10:13:27 +0200

    yml

---
+++ success.php
@@ -0,0 +1,127 @@
+ <!doctype html>
+<html class="no-js" lang="cs">
+    <head>
+        <title>Groww. weby jednoduše</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="author" content="David Fenynec">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+        <meta name="description" content="Tvoříme rychlé a spolehlivé webové stránky pro klienty z celého světa. Spusťte moderní web, který přivádí zákazníky.">
+        <!-- favicon icon -->
+        <link rel="shortcut icon" href="images/favicon.png">
+        <link rel="apple-touch-icon" href="images/apple-touch-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+        <!-- google fonts preconnect -->
+        <link rel="preconnect" href="https://fonts.googleapis.com" crossorigin>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <!-- style sheets and font icons  -->
+        <link rel="stylesheet" href="css/vendors.min.css"/>
+        <link rel="stylesheet" href="css/icon.min.css"/>
+        <link rel="stylesheet" href="css/style.css"/>
+        <link rel="stylesheet" href="css/responsive.css"/>
+        <link rel="stylesheet" href="demos/marketing/marketing.css" />
+    </head>
+    <body data-mobile-nav-style="classic" class="custom-cursor">
+        <!-- start cursor -->
+        <div class="cursor-page-inner">
+            <div class="circle-cursor circle-cursor-inner"></div>
+            <div class="circle-cursor circle-cursor-outer"></div>
+        </div>
+        <!-- end cursor -->
+        <!-- start header -->
+   <?php include 'assets/php/header.php';?>     
+ <!-- start footer -->¨
+
+
+ <!-- dekovaci-stranka.php -->
+
+
+
+<section class="pt-5">
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-lg-7 col-md-9">
+        <div class="bg-very-light-gray border-radius-6px p-50px lg-p-25px text-center box-shadow-large">
+          <span class="fs-32 alt-font fw-700 text-dark-gray mb-20px d-block">Děkujeme za platbu!</span>
+          <p class="fs-18 text-dark-gray mb-20px">
+            Vaše objednávka byla úspěšně dokončena.<br>
+            První měsíc správy reklam je zdarma.<br>
+            Brzy vás kontaktujeme ohledně nastavení přístupu k reklamním systémům Google Ads nebo Sklik.
+          </p>
+          <p class="fs-16 text-dark-gray mb-30px">
+            Pokud máte jakékoliv dotazy, neváhejte nás kontaktovat:<br>
+            <b>Telefon:</b> <a href="tel:608909981" class="text-dark-gray">608 909 981</a><br>
+            <b>Email:</b> <a href="mailto:dfenynec@gmail.com" class="text-dark-gray">dfenynec@gmail.com</a>
+          </p>
+          <a href="/" class="btn btn-dark-gray btn-large btn-switch-text btn-round-edge btn-box-shadow mt-10px">
+            <span>
+              <span class="btn-double-text" data-text="Zpět na hlavní stránku">Zpět na hlavní stránku</span>
+            </span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+      <?php include 'assets/php/footer.php'; ?>    
+          <!-- end footer -->
+        <!-- start subscription popup -->
+        <!-- (beze změny, skryto) -->
+        <!-- end subscription popup -->
+         <!-- start scroll progress -->
+        <div class="scroll-progress d-none d-xxl-block">
+          <a href="#" class="scroll-top" aria-label="scroll">
+            <span class="scroll-text">Scroll</span><span class="scroll-line"><span class="scroll-point"></span></span>
+          </a>
+        </div>
+        <!-- end scroll progress -->
+         <!-- start cookies model -->
+        <div id="cookies-model" class="cookie-message bg-dark-gray border-radius-8px" style="display: block;">
+            <div class="cookie-description fs-14 text-white mb-20px lh-22">
+                Cookies tu mám, aby všechno šlapalo jak má. A věřte - na vašem novém webu je určitě taky oceníte!
+            </div>
+            <div class="cookie-btn">
+                <a href="#" class="btn btn-transparent-white border-1 border-color-transparent-white-light btn-very-small btn-switch-text btn-rounded w-100 mb-15px accept_necessary_btn " aria-label="btn">
+                    <span>
+                        <span class="btn-double-text" data-text="jenom pár">Pouze nezbytné</span>
+                    </span>
+                </a>
+                <a href="#" class="btn btn-white btn-very-small btn-switch-text btn-box-shadow btn-rounded accept_all_btn w-100 mb-15px" data-accept-btn="" aria-label="text">
+                    <span>
+                        <span class="btn-double-text" data-text="všechny sušenky">Přijmout vše</span>
+                    </span>
+                </a>
+            </div>
+            <div class="fs-14 text-center align-items-center ">
+                <a class="text-decoration-line-bottom fs-12 cookie_policy_link" href="#">Více o Cookies</a>
+            </div>
+            
+        </div>
+
+
+        <!-- end cookies model -->
+        <!-- javascript libraries -->
+       <script>
+        function getConsentCookie() {
+            const match = document.cookie.match(/(^|;) ?cookie_consent=([^;]*)(;|$)/);
+            return match ? match[2] : null;
+        }
+
+        // Only load Google Analytics if user accepted all cookies
+        if (getConsentCookie() === 'all') {
+            // Insert your Google Analytics/gtag code here
+        }
+        </script>
+
+
+       
+        <!-- javascript libraries -->
+          <script src="js/cookie-consent.js"></script>
+        <script type="text/javascript" src="js/jquery.js"></script>
+        <script type="text/javascript" src="js/vendors.min.js"></script>
+        <script type="text/javascript" src="js/main.js"></script>
+    </body>
+    </html>
