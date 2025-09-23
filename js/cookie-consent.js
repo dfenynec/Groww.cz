8a85f360bcba2b7f704243e4751b30ac965a43ae
dfenynec <160599407+dfenynec@users.noreply.github.com>
2025-09-23 18:50:58 +0200

    GA4

--- js/cookie-consent.js
+++ js/cookie-consent.js
@@ -47,10 +47,33 @@
         sendConsentToSheetsDB(type);
     }
 
-    function enableAllCookies() {
-        // Initialize Google Analytics, marketing, etc.
-        // Example: load GA4 here if not already loaded
-    }
+ function enableAllCookies() {
+    // Google Analytics 4
+    var gaScript = document.createElement('script');
+    gaScript.async = true;
+    gaScript.src = "https://www.googletagmanager.com/gtag/js?id=G-G31QGDFGTW";
+    document.head.appendChild(gaScript);
+
+    gaScript.onload = function() {
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-G31QGDFGTW');
+    };
+
+  // Meta Pixel
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', 'TVŮJ_PIXEL_ID');
+    fbq('track', 'PageView');
+
+}
 
     function enableOnlyNecessaryCookies() {
         // Do not load analytics/marketing scripts
